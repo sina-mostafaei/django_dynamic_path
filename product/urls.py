f0d63570065f66f47265d1b78a286da70662f3b7
sina mostafaei <sina84mostafaei@gmail.com>
2026-02-14 12:53:20 +0330

    Initial Django project

---
+++ product/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.wel),
+    path('product/<id>', views.reporter),
+    path('product/<text>', views.mamad),
+]
---
+++ product/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+# Create your views here.
+
+products = {
+    "1": "phone",
+    "2": "laptop",
+    "3": "game"}
+
+texts = {
+    "1st": 1,
+    "2nd": 2,
+    "3rd": 3,
+}
+
+
+def wel(r):
+    return HttpResponse("welcome")
+
+
+def reporter(r, id):
+   
+    if id in products:
+        return HttpResponse(products[id])
+    else:
+        return HttpResponseNotFound("item not found")
+
+
+def mamad(request, text):
+    if text in texts:
+        return HttpResponseRedirect(f"/product/{str(texts[text])}/")
+    return HttpResponseNotFound("item not found")
